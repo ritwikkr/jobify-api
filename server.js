87ada3ba61e6c8ae71d2e04fccf2610b0e8d2be3
Ritwik <ritwikkumarrishu@gmail.com>
2023-06-17 22:52:23 +0530

    Added .env and project setup

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ const PORT = process.env.PORT || 5000;
 async function start() {
   try {
     await connectDB(process.env.MONGO_URI);
+    console.log(`Database Connected`);
     app.listen(PORT, () => {
       console.log(`Server is running at PORT: ${PORT}`);
     });
